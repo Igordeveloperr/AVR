e7d1e6f13248cea27dcce761f1b90178dc47647e
Igordeveloperr <g.bikmetow@gmail.com>
2023-07-22 14:38:02 +0300

    часы почти робят

--- 0_ПРАКТИКА/1_подключение_lcd1602/GccApplication1/GccApplication1/main.c
+++ 0_ПРАКТИКА/1_подключение_lcd1602/GccApplication1/GccApplication1/main.c
@@ -3,6 +3,8 @@
 #include <stdio.h>
 #define F_CPU 32768UL;
 
+int hour = 0;
+int minutes = 0;
 int seconds = 0;
 char str[80];
 
@@ -56,18 +58,39 @@ void clear_lcd()
 	execute_cmd();
 }
 
+void control_time()
+{
+	if (minutes == 60)
+	{
+		hour++;
+		minutes = 0;
+	}
+	
+	if (hour == 23 && minutes == 59 && seconds == 58)
+	{
+		hour = 0;
+		minutes = 0;
+		seconds = 0;
+	}
+}
+
 ISR(TIMER1_OVF_vect)
 {
-	PORTB |= (1 << PB0);
 	seconds += 2;
-	clear_lcd();
-	sprintf(str, "%d", seconds);
-	send_text(str);
+	if (seconds == 60)
+	{
+		minutes++;
+		seconds = 0;
+		control_time();
+		clear_lcd();
+		sprintf(str, "%d:%d", hour, minutes);
+		send_text(str);
+	}
+	TCNT1 = 0;
 }
 
 int main(void)
 {	
-	DDRB = (1 << PB0);
 	DDRC = (1 << PC0) | (1 << PC1) | (1 << PC2);
 	DDRD = 0b11111111;
 	lcd_init();
@@ -75,6 +98,9 @@ int main(void)
 	TIMSK |= (1 << TOIE1);
 	sei();
 	TCCR1B |= (1 << CS10);
+	
+	sprintf(str, "%d:%d", hour, minutes);
+	send_text(str);
 	while(1)
 	{
 		
