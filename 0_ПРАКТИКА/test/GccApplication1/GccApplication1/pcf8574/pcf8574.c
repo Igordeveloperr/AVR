30d24d8691237e358a336583462b48900abb7f5e
Igordeveloperr <g.bikmetow@gmail.com>
2023-09-18 21:30:48 +0300

    либа гготова

--- 0_ПРАКТИКА/test/GccApplication1/GccApplication1/pcf8574/pcf8574.c
+++ 0_ПРАКТИКА/test/GccApplication1/GccApplication1/pcf8574/pcf8574.c
@@ -1,27 +1,20 @@
 #include "../main.h"
-//path to i2c fleury lib
 #include PCF8574_I2CFLEURYPATH
 
-/*
- * initialize
- */
+// инициализация pcf
 void pcf8574_init() {
 	#if PCF8574_I2CINIT == 1
-	//init i2c
+	// инитим i2c
 	i2c_init();
 	_delay_us(10);
 	#endif
-
-	//reset the pin status
 	uint8_t i = 0;
 	for(i=0; i<PCF8574_MAXDEVICES; i++)
 		pcf8574_pinstatus[i] = 0;
 
 }
 
-/*
- * get output status
- */
+// получаем статус вывода
 int8_t pcf8574_getoutput(uint8_t deviceid) {
 	int8_t data = -1;
 	if((deviceid >= 0 && deviceid < PCF8574_MAXDEVICES)) {
@@ -30,9 +23,7 @@ int8_t pcf8574_getoutput(uint8_t deviceid) {
 	return data;
 }
 
-/*
- * get output pin status
- */
+// получаем статус пинов вывода
 int8_t pcf8574_getoutputpin(uint8_t deviceid, uint8_t pin) {
 	int8_t data = -1;
 	if((deviceid >= 0 && deviceid < PCF8574_MAXDEVICES) && (pin >= 0 && pin < PCF8574_MAXPINS)) {
@@ -42,9 +33,7 @@ int8_t pcf8574_getoutputpin(uint8_t deviceid, uint8_t pin) {
 	return data;
 }
 
-/*
- * set output pins
- */
+// настройка вывода
 int8_t pcf8574_setoutput(uint8_t deviceid, uint8_t data) {
 	if((deviceid >= 0 && deviceid < PCF8574_MAXDEVICES)) {
 		pcf8574_pinstatus[deviceid] = data;
@@ -56,16 +45,8 @@ int8_t pcf8574_setoutput(uint8_t deviceid, uint8_t data) {
 	return -1;
 }
 
-/*
- * set output pins, replace actual status of a device from pinstart for pinlength with data
- */
+// установить выходные контакты, заменить фактический статус устройства из pinstart для i2c
 int8_t pcf8574_setoutputpins(uint8_t deviceid, uint8_t pinstart, uint8_t pinlength, int8_t data) {
-	//example:
-	//actual data is         0b01101110
-	//want to change              ---
-	//pinstart                    4
-	//data                        101   (pinlength 3)
-	//result                 0b01110110
 	if((deviceid >= 0 && deviceid < PCF8574_MAXDEVICES) && (pinstart - pinlength + 1 >= 0 && pinstart - pinlength + 1 >= 0 && pinstart < PCF8574_MAXPINS && pinstart > 0 && pinlength > 0)) {
 	    uint8_t b = 0;
 	    b = pcf8574_pinstatus[deviceid];
@@ -75,7 +56,7 @@ int8_t pcf8574_setoutputpins(uint8_t deviceid, uint8_t pinstart, uint8_t pinleng
 		b &= ~(mask);
 		b |= data;
 	    pcf8574_pinstatus[deviceid] = b;
-	    //update device
+	    //рестартим
 		i2c_start(((PCF8574_ADDRBASE+deviceid)<<1) | I2C_WRITE);
 		i2c_write(b);
 		i2c_stop();
@@ -84,16 +65,14 @@ int8_t pcf8574_setoutputpins(uint8_t deviceid, uint8_t pinstart, uint8_t pinleng
 	return -1;
 }
 
-/*
- * set output pin
- */
+// настройка пинов вывода
 int8_t pcf8574_setoutputpin(uint8_t deviceid, uint8_t pin, uint8_t data) {
 	if((deviceid >= 0 && deviceid < PCF8574_MAXDEVICES) && (pin >= 0 && pin < PCF8574_MAXPINS)) {
 	    uint8_t b = 0;
 	    b = pcf8574_pinstatus[deviceid];
 	    b = (data != 0) ? (b | (1 << pin)) : (b & ~(1 << pin));
 	    pcf8574_pinstatus[deviceid] = b;
-	    //update device
+	    //рестартим
 		i2c_start(((PCF8574_ADDRBASE+deviceid)<<1) | I2C_WRITE);
 		i2c_write(b);
 		i2c_stop();
@@ -102,24 +81,17 @@ int8_t pcf8574_setoutputpin(uint8_t deviceid, uint8_t pin, uint8_t data) {
 	return -1;
 }
 
-/*
- * set output pin high
- */
+// установка высокого уровня на выходных пинах
 int8_t pcf8574_setoutputpinhigh(uint8_t deviceid, uint8_t pin) {
 	return pcf8574_setoutputpin(deviceid, pin, 1);
 }
 
-/*
- * set output pin low
- */
+// установка низкого уровня на выходных пинах
 int8_t pcf8574_setoutputpinlow(uint8_t deviceid, uint8_t pin) {
 	return pcf8574_setoutputpin(deviceid, pin, 0);
 }
 
-
-/*
- * get input data
- */
+// получение входных данных
 int8_t pcf8574_getinput(uint8_t deviceid) {
 	int8_t data = -1;
 	if((deviceid >= 0 && deviceid < PCF8574_MAXDEVICES)) {
@@ -130,9 +102,7 @@ int8_t pcf8574_getinput(uint8_t deviceid) {
 	return data;
 }
 
-/*
- * get input pin (up or low)
- */
+// получение входного контакта (высокий или низкий)
 int8_t pcf8574_getinputpin(uint8_t deviceid, uint8_t pin) {
 	int8_t data = -1;
 	if((deviceid >= 0 && deviceid < PCF8574_MAXDEVICES) && (pin >= 0 && pin < PCF8574_MAXPINS)) {
--- 0_ПРАКТИКА/test/GccApplication1/GccApplication1/pcf8574/pcf8574.h
+++ 0_ПРАКТИКА/test/GccApplication1/GccApplication1/pcf8574/pcf8574.h
@@ -1,31 +1,17 @@
-/*
-pcf8574 lib 0x02
-
-copyright (c) Davide Gironi, 2012
-
-Released under GPLv3.
-Please refer to LICENSE file for licensing information.
-*/
-
-
 #ifndef PCF8574_H_
 #define PCF8574_H_
 
-#define PCF8574_ADDRBASE (0x27) //device base address
+#define PCF8574_ADDRBASE (0x27) // адрес уст-ва
 
-#define PCF8574_I2CINIT 1 //init i2c
+#define PCF8574_I2CINIT 1 // инициализация i2c
 
-#define PCF8574_MAXDEVICES 1 //max devices, depends on address (3 bit)
-#define PCF8574_MAXPINS 8 //max pin per device
+#define PCF8574_MAXDEVICES 1 // макс кол-во утройств
+#define PCF8574_MAXPINS 8 // макс кол-во пинов
 
-//settings
-#define PCF8574_I2CFLEURYPATH "../i2c/i2cmaster.h" //define the path to i2c fleury lib
+#define PCF8574_I2CFLEURYPATH "../i2c/i2cmaster.h" // подкл i2c
 
-//pin status
 volatile uint8_t pcf8574_pinstatus[PCF8574_MAXDEVICES];
 
-
-//functions
 void pcf8574_init();
 extern int8_t pcf8574_getoutput(uint8_t deviceid);
 extern int8_t pcf8574_getoutputpin(uint8_t deviceid, uint8_t pin);
