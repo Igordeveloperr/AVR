59b2cb8ad170a82b3a1206004cbc6939445ee329
Igordeveloperr <g.bikmetow@gmail.com>
2024-01-26 15:55:35 +0300

    аппаратная часть визуализации готова

---
+++ 0_ПРАКТИКА/7_UART_PULT/7_UART_PULT/main.c
@@ -0,0 +1,107 @@
+#include <avr/io.h>
+#include <avr/delay.h>
+#include <avr/interrupt.h>
+#define F_CPU 8000000UL
+
+/*
+	асинхронный	 режим
+	пакет - 8 бит
+	бит четности - нет
+	один стоп бит
+	битрейт - 9600 BOD
+*/
+void UART_init(void)
+{
+	UBRR0L = 12;
+	UCSR0A |= (1 << U2X0);
+	UCSR0B |= (1 << RXEN0) | (1 << TXEN0);
+}
+
+/*
+	ждем освобождения регистра данных
+	записываем новое значение
+*/
+void UART_send(char data)
+{
+	while(!(UCSR0A&(1 << UDRE0)));
+	UDR0 = data;
+}
+
+/*
+	проверяем закончился ли прием данных
+	берем данные из регистра и возвращаем их
+*/
+uint8_t UART_read_com(void)
+{
+	uint8_t data = -1;
+	if(UCSR0A&(1 << RXC0))
+	{
+		data = UDR0;
+	}
+	return data;
+}
+
+/*
+	визуализация ошибки подключения сети
+*/
+void network_error_blink(void)
+{
+	PORTB |= (1 << PB0) | (1 << PB1);
+	_delay_ms(500);
+	PORTB &= ~(1 << PB0);
+	PORTB &= ~(1 << PB1);
+	_delay_ms(500);
+	PORTB |= (1 << PB0) | (1 << PB1);
+}
+
+/*
+	визуализация подключенной сети
+*/
+void activate_diode(void)
+{
+	PORTB &= ~(1 << PB0);
+	PORTB &= ~(1 << PB1);
+	uint8_t res = UART_read_com();
+	if (res == 48)
+	{
+		PORTB |= (1 << PB0);
+	}
+	else if (res == 49)
+	{
+		PORTB |= (1 << PB1);
+	}
+	else
+	{
+		network_error_blink();
+	}
+}
+
+/*
+	конфигурация МК перед стартом основного алгоритма
+*/
+void setup(void)
+{
+	DDRB |= (1 << PB0) | (1 << PB1);
+	UCSR0B |= (1 << RXCIE0);
+	sei();
+}
+
+/*
+	прерывание по входным данным, полученным через UART
+*/
+ISR(USART_RX_vect)
+{
+	activate_diode();
+}
+
+int main(void)
+{
+	setup();
+	network_error_blink();
+	UART_init();
+    while (1) 
+    {
+		
+    }
+}
+
